f1e9748dc18990a69547b06183c0893d426f2139
Ignacio <ignadiasg@gmail.com>
2026-04-14 19:52:41 -0300

    cache for follows

--- internal/service/following_service.go
+++ internal/service/following_service.go
@@ -3,9 +3,11 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"socialnet/internal/cache"
 	"socialnet/internal/database"
 	"socialnet/internal/models"
+	"time"
 )
 
 type FollowingService interface {
@@ -33,11 +35,25 @@ func (s *implFollowingService) CreateFollow(ctx context.Context, follow *models.
 	if follow.FollowerID == follow.FollowingID {
 		return errors.New("can't follow yourself")
 	}
-	return s.followingRepo.CreateFollow(ctx, follow)
+	if err := s.followingRepo.CreateFollow(ctx, follow); err != nil {
+		return err
+	}
+	followersCountKey := fmt.Sprintf("followersCount:%d", follow.FollowingID)
+	followingCountKey := fmt.Sprintf("followingCount:%d", follow.FollowerID)
+	_ = s.cache.Delete(followersCountKey)
+	_ = s.cache.Delete(followingCountKey)
+	return nil
 }
 
 func (s *implFollowingService) DeleteFollow(ctx context.Context, follow *models.Follow) error {
-	return s.followingRepo.DeleteFollow(ctx, follow)
+	if err := s.followingRepo.DeleteFollow(ctx, follow); err != nil {
+		return err
+	}
+	followersCountKey := fmt.Sprintf("followersCount:%d", follow.FollowingID)
+	followingCountKey := fmt.Sprintf("followingCount:%d", follow.FollowerID)
+	_ = s.cache.Delete(followersCountKey)
+	_ = s.cache.Delete(followingCountKey)
+	return nil
 }
 
 func (s *implFollowingService) GetFollowersFromID(ctx context.Context, userID int64, limit, offset int) ([]models.User, error) {
@@ -49,9 +65,37 @@ func (s *implFollowingService) GetFollowingFromID(ctx context.Context, userID in
 }
 
 func (s *implFollowingService) GetCountFollowers(ctx context.Context, userID int64) (int, error) {
-	return s.followingRepo.GetCantFollowersFromUserID(ctx, userID)
+	if userID <= 0 {
+		return 0, errors.New("invalid user ID")
+	}
+	key := fmt.Sprintf("followersCount:%d", userID)
+	var followersCount int
+	if err := s.cache.Get(key, &followersCount); err == nil {
+		return followersCount, nil
+	}
+
+	followersCount, err := s.followingRepo.GetCantFollowersFromUserID(ctx, userID)
+	if err != nil {
+		return 0, err
+	}
+	_ = s.cache.Set(key, followersCount, time.Hour)
+	return followersCount, nil
 }
 
 func (s *implFollowingService) GetCountFollowing(ctx context.Context, userID int64) (int, error) {
-	return s.followingRepo.GetCantFollowingsFromUserID(ctx, userID)
+	if userID <= 0 {
+		return 0, errors.New("invalid user ID")
+	}
+	key := fmt.Sprintf("followingCount:%d", userID)
+	var followingCount int
+	if err := s.cache.Get(key, &followingCount); err == nil {
+		return followingCount, nil
+	}
+
+	followingCount, err := s.followingRepo.GetCantFollowingsFromUserID(ctx, userID)
+	if err != nil {
+		return 0, err
+	}
+	_ = s.cache.Set(key, followingCount, time.Hour)
+	return followingCount, nil
 }
