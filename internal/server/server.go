74b4592cd96779aff2c78df857446a8f8f54f16e
Ignacio <ignadiasg@gmail.com>
2026-04-17 00:51:02 -0300

    server

---
+++ internal/server/server.go
@@ -0,0 +1,68 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"socialnet/internal/router"
+	"syscall"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+	router     *router.Router
+}
+
+func NewServer(port string, router *router.Router) *Server {
+	return &Server{
+		router: router,
+		httpServer: &http.Server{
+			Addr:         fmt.Sprintf(":%s", port),
+			ReadTimeout:  15 * time.Second,
+			WriteTimeout: 15 * time.Second,
+			IdleTimeout:  60 * time.Second,
+		},
+	}
+}
+
+func (s *Server) Initialize() error {
+	s.httpServer.Handler = s.router.SetupRoutes()
+
+	log.Printf("Server starting on %s", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	log.Println("Shutting down server...")
+	return s.httpServer.Shutdown(ctx)
+}
+
+func (s *Server) StartServer(router router.Router) {
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := s.Initialize(); err != nil {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+	}()
+
+	log.Println("Server started successfully")
+
+	<-done
+	log.Println("Server stopping...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server stopped")
+}
